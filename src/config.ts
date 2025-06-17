a93fe285e40e85733aaf0096271fac60a0b1a386
DannyAkintunde <dannyakintundelct@gmail.com>
2025-06-17 19:03:19 +0100

    #4 Update README.md

--- src/config.ts
+++ src/config.ts
@@ -2,10 +2,10 @@ import "dotenv/config";
 
 const config = {
     sessionTimeout: parseInt(process.env.SESSION_TIMEOUT || "604800"), // 7 days
-    databaseUrl: process.env.DATABASE_URL || "",
-    encryptionKey: process.env.ENCRYPTION_KEY || "",
+    databaseUrl: process.env.DATABASE_URL || "", // Redis database URL (required)
+    encryptionKey: process.env.ENCRYPTION_KEY || "", // Encryption key (must be a 32-byte base64-encoded buffer, generate using crypto)
     port: parseInt(process.env.PORT || "8000"),
-    prefix: process.env.PREFIX || "DEVD",
+    prefix: process.env.PREFIX || "DEVD", // prefix on session id
 };
 
 export default Object.freeze(config);
